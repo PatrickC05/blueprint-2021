8cdf0b0f35f5b300c299b86dd84275cd6034f1f2
Dynosol <64035706+Dynosol@users.noreply.github.com>
2021-02-21 14:52:47 -0500

    Added in static + templates
    And changed some settings

--- stocks/showForcast/views.py
+++ stocks/showForcast/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hi")
+    return render(request, "home.html")
 
 def get_prices(request, ticker):
     l = [i/100 for i in range(30)]
