77c41c7e067d8cce2f4b4015762bbf99109379b7
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-02-21 17:18:12 -0500

    fixing showprice

--- stocks/showForcast/views.py
+++ stocks/showForcast/views.py
@@ -7,7 +7,7 @@ def index(request):
 
 def get_prices(request, ticker):
     if ticker:
-        l = [i/100 for i in range(30)]
+        l = [i/100 for i in range(37)]
         return JsonResponse({"prices":l})
     return JsonResponse({"error": "stock does not exist"},status=404)
 
--- stocks/stocks/static/js/showPrice.js
+++ stocks/stocks/static/js/showPrice.js
@@ -7,9 +7,14 @@ fetch(`/prices/{ticker}`)
     x: [-29, -28, -27, -26, -25, -24, -23, -22, -21, -20, -19, -18, -17, -16, -15, -14, -13, -12, -11,
     -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7],
     y: result['prices'],
-    type: 'scatter'
+    type: 'scatter',
+    mode: 'lines',
+    line: {
+        color: 'rgb(0,0,0)',
+        width: 5
+      }
   };
-
+  console.log(trace1)
   var data = [trace1];
   var layout = {
     title:'Past and Predicted Price',
@@ -19,9 +24,13 @@ fetch(`/prices/{ticker}`)
     },
     yaxis: {
       title: 'Price',
-    }
+    },
+
+    xaxis: {
+    range: [-30, 7]
+  },
   };
 
-  Plotly.newPlot('graph', data, layout);
+  Plotly.plot('graph', data,layout);
 });
 });
