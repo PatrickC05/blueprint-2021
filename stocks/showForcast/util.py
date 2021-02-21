1926b27721ca8ec6afaf6d9604d42fe7686ed68b
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-02-21 16:10:11 -0500

    yfinance added

--- stocks/showForcast/util.py
+++ stocks/showForcast/util.py
@@ -1,10 +1,29 @@
 import yfinance
 import datetime
 import pandas as pd
+from tensorflow import lite
+import numpy as np
+interpreter = lite.Interpreter(model_path="model.tflite")
+
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 def getPrices(ticker):
     date = datetime.datetime.today()
     print(date)
-    l = yfinance.download(ticker,date-datetime.timedelta(days=30),date,progress=False)
-    prices = list(l['Close'])
-    return prices
+    l = yfinance.download(ticker,date-datetime.timedelta(days=50),date,progress=False)
+    prices = list(l['Close'])[-30:]
+    input_shape = input_details[0]['shape']
+    prices_array = np.array(prices)
+    prices_array = prices_array[np.newaxis,...,np.newaxis]
+    print(prices_array.shape)
+    interpreter.set_tensor(
+        input_details[0]['index'], prices_array)
+
+    interpreter.invoke()
+
+    output_days = interpreter.get_tensor(output_details[0]['index'])
+    return output_days
