4b318e1846c6a1ebd48458097bf85b894e4b2a0a
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-02-21 17:23:37 -0500

    plotly not working

--- stocks/stocks/static/js/showPrice.js
+++ stocks/stocks/static/js/showPrice.js
@@ -4,18 +4,14 @@ fetch(`/prices/{ticker}`)
 .then((result) => {
   console.log(result)
   var trace1 = {
-    x: [-29, -28, -27, -26, -25, -24, -23, -22, -21, -20, -19, -18, -17, -16, -15, -14, -13, -12, -11,
-    -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7],
+    x: [1, 2, 3, 4],
     y: result['prices'],
-    type: 'scatter',
-    mode: 'lines',
-    line: {
-        color: 'rgb(0,0,0)',
-        width: 5
-      }
+    mode: 'markers',
+    type: 'scatter'
   };
-  console.log(trace1)
+
   var data = [trace1];
+
   var layout = {
     title:'Past and Predicted Price',
 
@@ -31,6 +27,6 @@ fetch(`/prices/{ticker}`)
   },
   };
 
-  Plotly.plot('graph', data,layout);
+  Plotly.newPlot('graph', data);
 });
 });
