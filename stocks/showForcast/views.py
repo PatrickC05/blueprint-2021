56000909adb04b8a777e51b953e8bbf75792b7d8
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-02-21 13:29:53 -0500

    dummy get_prices working

--- stocks/showForcast/views.py
+++ stocks/showForcast/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hi")
+
+def get_prices(request, ticker):
+    l = [i/100 for i in range(30)]
+    return JsonResponse({"prices":l})
