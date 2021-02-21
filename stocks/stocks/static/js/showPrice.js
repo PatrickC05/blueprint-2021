1d8b011a4b88ed58baafa91b3fe88be7462a2640
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-02-21 18:16:06 -0500

    current and predicted prices shown

--- stocks/stocks/static/js/showPrice.js
+++ stocks/stocks/static/js/showPrice.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch("/prices/" + ticker)
     .then((response) => response.json())
     .then((result) => {
-      console.log(result['prices'])
+      console.log(result['prices']);
+      prices = result['prices'];
+      document.querySelector('#current_price').textContent = prices[29].toFixed(2);
+      document.querySelector('#predicted_price').textContent = prices[36].toFixed(2);
       var xvals = [];
-
       for (var i = 1; i <= 37; i++) {
         xvals.push(i);
       }
